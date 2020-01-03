4d64eea19f2c93d1df3bcc2db4ef4c4a9b91ad95
JasKay <iamjasanyakay@gmail.com>
2020-01-03 03:49:25 -0500

    commit

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,10 +1,8 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for(var i=0; i < numberOfDrumButtons; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
-}
-
-function handleClick(){
-    alert("clicked");
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+});
 }
