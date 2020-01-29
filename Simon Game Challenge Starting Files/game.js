1a143e1cfdfb9a76ada30b7a85fb4ddc86b4d077
JasKay <iamjasanyakay@gmail.com>
2020-01-29 14:21:15 -0500

    send

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,10 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+    var randomNumber = Math.random();  //creating a random number
+    randomNumber = randomNumber * 4;   //making it be between 0 and 3
+    randomNumber = Math.floor(n);
+
+}
+
+var randomChosenColour = 
